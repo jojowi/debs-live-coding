fe797962e45c3c79094fb73ca5ba02855d625948
Johannes Wirth <jojowi@gmx.net>
2019-06-24 15:05:03 +0200

    Use map.list instead of Var

--- chat/src/main/scala/chat/Application.scala
+++ chat/src/main/scala/chat/Application.scala
@@ -8,30 +8,23 @@ import rescala._
 
 @multitier
 object Application {
-  @peer type Peer
-  @peer type Server <: Peer { type Tie <: Multiple[Client] }
-  @peer type Client <: Peer { type Tie <: Single[Server] }
+  @peer type Server <: { type Tie <: Multiple[Client] }
+  @peer type Client <: { type Tie <: Single[Server] }
 
   val ui: Local[Frontend] on Client
 
-  val messages = on[Server] { Var(List[String]()) }
+  val messages = on[Server] {
+    (message.asLocalFromAllSeq map {
+      case (_, message) => message
+    }).list
+  }
 
   val message = on[Client] { ui.message }
 
-  def main(): Unit on Peer =
-    (
-      on[Server] {
-        message.asLocalFromAllSeq observe { case (index,message) =>
-          messages.transform(messages => message :: messages)
-        }
+  def main(): Unit on Client =
+    on[Client] {
+      messages.asLocal observe { list =>
+        ui.setMessages(list)
       }
-
-      and on[Client] {
-        messages.asLocal observe { list =>
-          ui.setMessages(list)
-        }
-      }
-    )
-
-
+    }
 }
